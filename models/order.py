96c29873821b5650eaea847426b43acbd1e9382c
Egba Frank <Egbafrank72@gmail.com>
2024-09-07 13:44:47 +0000

    feat: change associations from tables to models

--- models/order.py
+++ models/order.py
@@ -4,26 +4,55 @@ Contains implementation for the order class
 from os import getenv
 from .base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from sqlalchemy import Column, String, Integer
+from sqlalchemy import ForeignKey, Table, CheckConstraint
 
 
-if getenv("GH_STORAGE_TYPE") == "db":
-    order_product = Table(
-            'order_product', Base.metadata,
-            Column('order_id', String(60),
-                   ForeignKey('orders.id', onupdate='CASCADE',
-                              ondelete='CASCADE'),
-                   primary_key=True),
-            Column('product_id', String(60),
-                   ForeignKey('products.id', onupdate='CASCADE',
-                              ondelete='CASCADE'),
-                   primary_key=True),
-            Column('quanity', Integer, default=1)
-    )
+GH_STORAGE_TYPE = getenv("GH_STORAGE_TYPE")
+
+if GH_STORAGE_TYPE == "db":
+    class Order_product(Base):
+        """Model for managing product quantities in orders"""
+        __tablename__ = "order_products"
+
+        order_id = Column(
+                String(60),
+                ForeignKey('orders.id',
+                           ondelete='CASCADE',
+                           onupdate='CASCADE'
+                           ),
+                primary_key=True
+                )
+
+        product_id = Column(
+                String(60),
+                ForeignKey('products.id',
+                           ondelete='CASCADE',
+                           onupdate='CASCADE'
+                           ),
+                primary_key=True
+                )
+
+        quantity = Column(
+                Integer,
+                CheckConstraint('quantity > 0'),
+                default=1
+                )
+
+        order = relationship(
+                'Order',
+                back_populates='products'
+                )
+        product = relationship('Product')
+
+        def __init__(self, order_id, product_id, quantity=1):
+            self.order_id = order_id
+            self.product_id = product_id
+            self.quantity = quantity
 
 
 class Order(BaseModel, Base):
-    if getenv("GH_STORAGE_TYPE") == "db":
+    if GH_STORAGE_TYPE == "db":
         __tablename__ = "orders"
 
         status = Column(String(60), nullable=False)
@@ -38,9 +67,9 @@ class Order(BaseModel, Base):
                 )
 
         products = relationship(
-                'Product',
-                secondary="order_product",
-                backref='order',
+                'Order_product',
+                back_populates='order',
+                cascade='all, delete-orphan'
         )
 
     else:
