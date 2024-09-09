713ea82065819b43806cd539ca7e18f23923bf8e
Egba Frank <Egbafrank72@gmail.com>
2024-09-09 05:53:01 +0000

    feat (#8): implement web framework

--- web_flask/app.py
+++ web_flask/app.py
@@ -7,35 +7,57 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def close_db(execption):
     """ Close current db session """
     storage.close()
+
 
 @app.route("/", strict_slashes=False)
 def login():
     """ Returns the login page """
     return render_template("index.html")
 
-@app.route("/grocery_list", strict_slashes=False)
-def shop_list():
+
+@app.route("/grocery_list/<list_id>", strict_slashes=False)
+def shop_list(list_id=None):
     """ Return grocery_list page """
-    return render_template("grocery_list.html")
+    from models.shop_list import Shop_list
+    user_list = storage.get(Shop_list, list_id)
+    return render_template(
+            "grocery_list.html",
+            user_list=user_list
+            )
+
 
 @app.route("/user_profile", strict_slashes=False)
 def user_profile():
     """ Returns the user_profile page """
     return render_template("user_profile.html")
 
+
 @app.route("/product_display", strict_slashes=False)
 def product_display():
     """ Returns the product_display page"""
-    return render_template("product_display.html")
+    from models.category import Category
+    categories = storage.all(Category).values()
+    return render_template(
+            "product_display.html",
+            categories=categories
+            )
+
 
 @app.route("/product_search", strict_slashes=False)
 def product_search():
     """ Return the product_search page """
-    return render_template("product_search.html")
+    from models.shop import Shop
+    shops = storage.all(Shop).values()
+    return render_template(
+            "product_search.html",
+            shops=shops
+            )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
