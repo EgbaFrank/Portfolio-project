24a46325110cbcefbe8f3b28d910846f123b1284
Egba Frank <Egbafrank72@gmail.com>
2024-09-14 12:38:20 +0000

    docs: add api documentation for api status and stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,14 +12,17 @@ from models.shop_list import Shop_list
 from models.place import Place
 from models.product import Product
 from models.user import User
+from flasgger import swag_from
 
 
 @app_views.route('/status', strict_slashes=False)
+@swag_from("api_docs/status.yaml")
 def status():
     """ Returns API status """
     return jsonify({"status": "OK"})
 
 @app_views.route('/stats', strict_slashes=False)
+@swag_from("api_docs/stats.yaml")
 def stats():
     """ Retrieves the number of each instance """
     classes = [Category, Order, Shop, Shop_list, Place, Product, User]
