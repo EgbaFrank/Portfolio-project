7b5dd91bd56bd42563f6056a8f305e22ae9de1cf
Egba Frank <Egbafrank72@gmail.com>
2024-09-19 20:06:44 +0000

    feat: implement search and add-to-list buttons functionalities

--- web_flask/static/scripts/product_display.js
+++ web_flask/static/scripts/product_display.js
@@ -3,7 +3,33 @@ document.addEventListener('DOMContentLoaded', () => {
 	buttons.forEach((button) => {
 		button.addEventListener('click', () => {
 			const productId = button.dataset.id;
+			const testListId = "3de5f04b-06f1-4aac-8647-5aeba3dc29b0"
 			console.log(`Button clicked for product ID: ${productId}`);
+			url = `http://127.0.0.1:5000/api/v1/shop_lists/${testListId}/products/${productId}`
+			fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({})
+			})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
+			})
+			.catch(error => console.error(error));
 		});
+	});
+
+	const searchInput = document.getElementById('search-input');
+	const searchButton = document.getElementById('search-button');
+
+	searchButton.addEventListener('click', () => {
+		const searchQuery = searchInput.value.trim();
+		if (searchQuery !== '') {
+			const formattedQuery = encodeURIComponent(searchQuery);
+			window.location.href = `/product_search?name=${formattedQuery}`;
+		}
 	});
 });
